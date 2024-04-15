a2dd0103d5d8b679e0c0465829f99623ac0586ca
MarufiMac <marufalam120@gmail.com>
2024-04-16 00:43:33 +0600

    add nav drawer

--- app/src/main/java/com/maruf/mmusicplayer/MainActivity.kt
+++ app/src/main/java/com/maruf/mmusicplayer/MainActivity.kt
@@ -3,21 +3,31 @@ package com.maruf.mmusicplayer
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.maruf.mmusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
+  private lateinit var toggle: ActionBarDrawerToggle
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    // enableEdgeToEdge()
+    //enableEdgeToEdge()
     requestRuntimePermission()
     setTheme(R.style.Theme_MMusicPlayer)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+   //for nav drawer
+    toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+    binding.root.addDrawerListener(toggle)
+    toggle.syncState()
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.show()
 
     binding.apply {
       shuffleBtn.setOnClickListener {
@@ -55,5 +65,10 @@ class MainActivity : AppCompatActivity() {
           this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
       }
     }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (toggle.onOptionsItemSelected(item)) return true
+    return super.onOptionsItemSelected(item)
   }
 }
