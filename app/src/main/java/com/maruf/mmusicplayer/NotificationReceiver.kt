c7c0c723795d82511f84abe8ab39014e63f787a8
MarufiMac <marufalam120@gmail.com>
2024-04-22 12:54:41 +0600

    now playing song bug fix

--- app/src/main/java/com/maruf/mmusicplayer/NotificationReceiver.kt
+++ app/src/main/java/com/maruf/mmusicplayer/NotificationReceiver.kt
@@ -1,6 +1,5 @@
 package com.maruf.mmusicplayer
 
-import android.app.Service.STOP_FOREGROUND_REMOVE
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,7 +8,6 @@ import com.maruf.mmusicplayer.PlayerActivity.Companion.binding
 import com.maruf.mmusicplayer.PlayerActivity.Companion.musicService
 import com.maruf.mmusicplayer.util.Utils.exitApplication
 import com.maruf.mmusicplayer.util.Utils.setSongPosition
-import kotlin.system.exitProcess
 
 class NotificationReceiver : BroadcastReceiver() {
   override fun onReceive(content: Context?, intent: Intent?) {
@@ -29,6 +27,7 @@ class NotificationReceiver : BroadcastReceiver() {
       musicService?.mediaPlayer?.start()
       musicService?.showNotification(R.drawable.pause_icon)
       binding.playPauseBtnPA.setIconResource(R.drawable.pause_icon)
+      NowPlayingFragment.binding.playPauseBtnNP.setIconResource(R.drawable.pause_icon)
     }
   }
 
@@ -38,6 +37,7 @@ class NotificationReceiver : BroadcastReceiver() {
       musicService?.mediaPlayer?.pause()
       musicService?.showNotification(R.drawable.play_icon)
       binding.playPauseBtnPA.setIconResource(R.drawable.play_icon)
+      NowPlayingFragment.binding.playPauseBtnNP.setIconResource(R.drawable.play_icon)
     }
   }
 
@@ -52,6 +52,12 @@ class NotificationReceiver : BroadcastReceiver() {
       error(R.mipmap.ic_music_player_icon)
     }
     binding.songNamePA.text = PlayerActivity.musicListPA[PlayerActivity.songPosition].title
+    NowPlayingFragment.binding.songImgNP.load(PlayerActivity.musicListPA[PlayerActivity.songPosition].artUri) {
+      crossfade(true)
+      placeholder(R.mipmap.ic_music_player_icon)
+      error(R.mipmap.ic_music_player_icon)
+    }
+    NowPlayingFragment.binding.songNameNP.text = PlayerActivity.musicListPA[PlayerActivity.songPosition].title
 
     playMusic()
   }
---
+++ app/src/main/java/com/maruf/mmusicplayer/NowPlayingFragment.kt
@@ -0,0 +1,79 @@
+package com.maruf.mmusicplayer
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import coil.load
+import com.maruf.mmusicplayer.databinding.FragmentNowPlayingBinding
+import com.maruf.mmusicplayer.util.Utils
+
+class NowPlayingFragment : Fragment() {
+  @SuppressLint("StaticFieldLeak")
+  companion object {
+    lateinit var binding: FragmentNowPlayingBinding
+  }
+
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View? {
+    val view = inflater.inflate(R.layout.fragment_now_playing, container, false)
+    binding = FragmentNowPlayingBinding.bind(view)
+    binding.root.visibility = View.INVISIBLE
+    binding.playPauseBtnNP.setOnClickListener {
+      if (PlayerActivity.isSongPlaying) pauseMusic() else playMusic()
+    }
+    binding.nextBtnNP.setOnClickListener {
+      nextMusic()
+    }
+    return view
+  }
+
+  override fun onResume() {
+    super.onResume()
+    if (PlayerActivity.musicService != null) {
+      binding.root.visibility = View.VISIBLE
+      binding.songImgNP.load(PlayerActivity.musicListPA[PlayerActivity.songPosition].artUri) {
+        crossfade(true)
+        placeholder(R.mipmap.ic_music_player_icon)
+        error(R.mipmap.ic_music_player_icon)
+      }
+      binding.songNameNP.text = PlayerActivity.musicListPA[PlayerActivity.songPosition].title
+      if (PlayerActivity.isSongPlaying)
+          binding.playPauseBtnNP.setIconResource(R.drawable.pause_icon)
+      else binding.playPauseBtnNP.setIconResource(R.drawable.play_icon)
+    }
+  }
+
+  private fun playMusic() {
+    PlayerActivity.isSongPlaying = true
+    PlayerActivity.musicService!!.mediaPlayer!!.start()
+    binding.playPauseBtnNP.setIconResource(R.drawable.pause_icon)
+    PlayerActivity.musicService!!.showNotification(R.drawable.pause_icon)
+  }
+
+  private fun pauseMusic() {
+    PlayerActivity.isSongPlaying = false
+    PlayerActivity.musicService!!.mediaPlayer!!.pause()
+    binding.playPauseBtnNP.setIconResource(R.drawable.play_icon)
+    PlayerActivity.musicService!!.showNotification(R.drawable.play_icon)
+  }
+
+  private fun nextMusic() {
+    Utils.setSongPosition(true)
+    PlayerActivity.musicService?.createMediaPlayer()
+    binding.songImgNP.load(PlayerActivity.musicListPA[PlayerActivity.songPosition].artUri) {
+      crossfade(true)
+      placeholder(R.mipmap.ic_music_player_icon)
+      error(R.mipmap.ic_music_player_icon)
+    }
+    binding.songNameNP.text = PlayerActivity.musicListPA[PlayerActivity.songPosition].title
+    PlayerActivity.musicService?.showNotification(R.drawable.pause_icon)
+    playMusic()
+
+  }
+}
